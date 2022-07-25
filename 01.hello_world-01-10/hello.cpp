37ce11732345e951cd2dd074e1a4c75bdd980a92
dh-i <dhanush.code1@gmail.com>
2022-07-25 18:18:30 +0530

    Day 1 hello_world(1-10)

---
+++ 01.hello_world-01-10/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//we can import the package as 
+//using namespace std;
+
+//or we can import individual functions as 
+//using std::cout;
+//using std::cin;
+
+int main(){
+    std::cout << "Hello world \n" << std::endl;
+    return EXIT_SUCCESS; //this is same as return 0;
+}
+
+//resources
+//https://en.cppreference.com/w/
+//https://cplusplus.com/
+//https://isocpp.org/
